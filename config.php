2af12b9e87fff9648102fc1ac2b0db9235b800b0
Pragnesh Mayani <69343111+pragnesh107@users.noreply.github.com>
2023-10-04 17:15:49 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+$servername = "localhost";
+$database = "artisan management system";
+$username = "root";
+$password = "";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+?>
